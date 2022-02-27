d13322a85d7090aeaf9c75bfe841c678b6a48edb
JJosan <nosajn123@gmail.com>
2022-02-26 19:34:35 -0800

    started new design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React from 'react';
 import './App.css';
-import Navbar from './Sections/Navbar.js';
-import SectionContainer from './Sections/SectionContainer.js';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar.js';
+import Professional from './Components/Professional/Professional.js';
+import Personal from './Components/Personal/Personal.js';
+
+// do some things with react reveal to make it look cooler
 
 function App() {
   return (
-    <div>
+    <div className='app'>
       <Navbar />
-      <SectionContainer />
+      <div className='app_body'>
+        <Routes>
+          <Route exact path='/Personal-Website/' element={ <Professional /> }/>
+          <Route exact path='/Personal-Website/personal/' element={ <Personal /> }/>
+        </Routes>
+      </div>
+      
+
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Navbar.css';
+import { Link, NavLink } from 'react-router-dom';
+
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div>
+        <div className='navbar_title'>
+          <h1 className='navbar_title_name'>Jason Nguyen</h1>
+          <h1 className='navbar_title_position'>University Student</h1>
+        </div>
+        <nav className='navbar_links'>
+          <div>
+            <NavLink to='/Personal-Website/'>Professional</NavLink>
+            <NavLink to='/Personal-Website/personal/'>Personal</NavLink>
+          </div>
+          <div>
+            <Link to='/Personal-Website/'>Resume</Link>
+            <Link to='/Personal-Website/'>Linkedin</Link>
+            <Link to='/Personal-Website/'>Github</Link>
+            <Link to='/Personal-Website/'>Contact</Link>
+          </div>
+        </nav>
+      </div>
+    </div>
+      
+  );
+}
+
+export default Navbar;
