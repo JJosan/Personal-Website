b5611e7feda8c2b89f7cd7962f93c5a558dbcb55
JJosan <nosajn123@gmail.com>
2022-06-11 17:19:30 -0700

    started on api fetching

--- src/Components/Personal/Summary/Summary.js
+++ src/Components/Personal/Summary/Summary.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Summary.css';
 import Fade from 'react-reveal/Fade';
 
+// const getData = () => {
+//   return fetch("https://tranquil-fortress-29998.herokuapp.com/").then(response => response.json())
+// }
+
 function Summary() {
+
+  const[things, setThings] = useState({})
+
+  useEffect(() => {
+    getData().then(data => setThings(data))
+  },[])
+
   return (
     <Fade bottom>
       <div className='summary'>
@@ -10,10 +21,22 @@ function Summary() {
           <hr />
           <h1>About Me</h1>
         </div>
-        <div className='summary_text'>
-          <p>Sleep is good for the mind</p>
-          <p>Food is good for the soul</p>
+        <div className='summary_body'>
+          <div className='summary_text'>
+            <p>Sleep is good for the mind</p>
+            <p>Food is good for the soul</p>
+            <p>Air Force 1s are good for self esteem</p>
+          </div>
+          <div className='summary_text'>
+            <p>Nike Shoes</p>
+            {/* {Object.entries(things).map(([key, value]) => {
+              return (
+                <p key={key}>{value}</p>
+              )
+            })} */}
+          </div>
         </div>
+        
       </div>
     </Fade>
   );
